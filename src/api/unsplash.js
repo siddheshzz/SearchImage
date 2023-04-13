5424dd9689d8a69b0e1f7361c170924fcd16f3ec
siddhesh zope <siddheshzope1023@gmail.com>
2023-04-13 15:53:25 +0530

    check

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const unsplash = async(term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
-            Authorization: 'Client-ID 5N5A32_aQjsHG9Vikopf6CZla8Z-F58C2w_rnFP0PFE'
+            Authorization: ''
         },
         params: {
             query: term,
